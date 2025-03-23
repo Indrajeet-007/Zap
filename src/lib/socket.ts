f0ccddd017308a3c11c66a4625c5ae0dde0b3fad
aspizu <aspizu@protonmail.com>
2025-03-23 20:11:14 +0530

    fix: update socket server URL to use environment variable

--- src/lib/socket.ts
+++ src/lib/socket.ts
@@ -1,8 +1,5 @@
 import { io } from "socket.io-client";
 
-// Change this to match your server URL if needed
-const SOCKET_SERVER_URL = "http://10.12.2.44:5000";
-
-export const socket = io(SOCKET_SERVER_URL, {
+export const socket = io(import.meta.env.VITE_SOCKET_URL, {
   transports: ["websocket"], // Ensures WebSocket is used
 });
